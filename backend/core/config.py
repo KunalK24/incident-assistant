ea231bfef04455d5834b3a902fe3e58a1e6343d3
KunalK24 <kunaldk98@gmail.com>
2026-05-07 15:44:40 -0700

    Initial Setup for app

---
+++ backend/core/config.py
@@ -0,0 +1,18 @@
+from pydantic_core import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    app_env: str = "development"
+
+    database_url: str
+    redis_url: str
+
+    openai_api_key: str
+    openai_chat_model: str = "gpt-4.1-mini"
+    openai_embedding_model: str = "text-embedding-3-small"
+
+    local_storage_path: str = "./storage/uploads"
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+settings = Settings()
